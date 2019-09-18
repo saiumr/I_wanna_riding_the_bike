694f00a0dfb09917ba0a16b947bd0a1c81fd0092
LiXianFeng <1797673894@qq.com>
2019-09-18 23:37:39 +0800

    added cmake structure on Windows

--- I_wanna_riding_the_bike/test/tlog.c
+++ I_wanna_riding_the_bike/test/tlog.c
@@ -2,7 +2,7 @@
 #include "unittest.h"
 #include "log.h"
 
-int main(){
+int main(int argc, char** argv){
     log_level level;
     setLogLevel(LOG_ERROR);
     level = getLogLevel();
--- Log.md
+++ Log.md
@@ -6,3 +6,6 @@
     * 完成了用于测试的游戏主体文件main.c
     * unittest.h单元测试系统测试完毕，确认无误
     * log.h日志系统已经通过单元测试完毕，确认无误
+
+**saiumr**:
+    * 增加了cmake工程在windows下的编译功能，解决了在windows下发现不了SDL2的错误
